06f9c29335e62d8e87c1f09f74d533774e7958df
Erdiawan Anna <erdie.one@gmail.com>
2019-12-23 17:32:57 +0700

    update image

--- components/Hero.vue
+++ components/Hero.vue
@@ -17,7 +17,7 @@
                     <p class="desc">I love being a part of community. Now i manage a community called GDG Jogjakarta as a Co-Manager. GDG Jogjakarta is Google Developer Group community based in Yogyakarta.</p>
                 </div>
                 <div class="hero-image">
-                    <img src="~/assets/img/erdi-color.jpeg" class="lazyload" alt="Anna Erdiawan">
+                    <img src="~/assets/img/erdi.jpg" class="lazyload" alt="Anna Erdiawan">
                     <div class="btn-hire">
                         <a href="mailto:hi@erdiawan"><button class="btn btn-primary">Hire Me</button></a>
                     </div>
