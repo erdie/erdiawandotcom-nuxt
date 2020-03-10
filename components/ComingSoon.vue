b836be06592c82e9bc44a69931748fd7e2608410
Erdiawan Anna <erdie.one@gmail.com>
2020-03-10 13:16:37 +0700

    add coming soon components

---
+++ components/ComingSoon.vue
@@ -0,0 +1,10 @@
+<template>
+    <section class="coming-soon">
+        <div class="container">
+            <div class="coming-soon-group">
+                <h1>Updated Soon!</h1>
+                <p><a href="/">&lt; Back to homepage</a></p>
+            </div>
+        </div>
+    </section>
+</template>
--- pages/journal/index.vue
+++ pages/journal/index.vue
@@ -1,43 +1,25 @@
 <template>
-    <div>
-        <section id="blog">
-            <div class="container">
-                <div class="blog item">
-                    <div class="d:item__4 t:item__6 m:item__12" v-for="post in posts" :key="post.id">
-                        <!-- <a v-bind:href="post.link"> -->
-                            <div class="blog-card">
-                                <!-- <nuxt-link :to="'/journal/'+post.slug"> -->
-                                <nuxt-link :to="{ name: 'journal-id', params: { id: post.id, slug: post.slug } }">
-                                <img v-bind:src="post.jetpack_featured_media_url" alt="">
-                                </nuxt-link>
-                                <div class="blog-title">
-                                    <h2>{{post.title.rendered}}</h2>
-                                </div>
-                                <div class="blog-author">
-                                    <small><strong>Posted on </strong> {{post.date_gmt.substring(0,10)}}</small>
-                                </div>
-                                <div class="blog-desc">
-                                    <p>{{post.excerpt.rendered.substring(0,200)+".."}}</p>
-                                </div>
-                            </div>
-                        <!-- </a> -->
-                    </div>
-                </div>
-            </div>
-        </section>
-    </div>
+  <div>
+    <Navigation />
+    <NavigationMobile />
+    <ComingSoon />
+    <Footer />
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
+import Navigation from '~/components/Navigation.vue'
+import NavigationMobile from '~/components/Navigation-Mobile.vue'
+import ComingSoon from '~/components/ComingSoon.vue'
+import Footer from '~/components/Footer.vue'
 
 export default {
-    layout: 'journal',
-    async asyncData() {
-        const { data } = await axios.get('https://chromplex.com/wp-json/wp/v2/posts') 
-        return {
-            posts: data
-        }
-    },
+  components: {
+    Navigation,
+    NavigationMobile,
+    ComingSoon,
+    Footer
+  }
 }
 </script>
+
