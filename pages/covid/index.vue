a44f1ad7913da2d6c8e5a1dd2c7b18eab04aef2c
Erdiawan Anna <erdie.one@gmail.com>
2020-03-20 00:00:24 +0700

    update style for covid page

--- pages/covid/index.vue
+++ pages/covid/index.vue
@@ -3,14 +3,15 @@
         <div class="container">
         <h2>Kasus Covid19 di Indonesia</h2>
         <ul>
-            <li>Terkonfirmasi: <b>{{alldata.confirmed.value}}</b></li>
-            <li>Dalam Perawatan: <b>{{alldata.active.value}}</b></li>
-            <li>Sembuh: <b>{{alldata.recovered.value}}</b></li>
-            <li>Meninggal: <b>{{alldata.deaths.value}}</b></li>
+            <li class="confirmed">Terkonfirmasi: <b>{{alldata.confirmed.value}}</b></li>
+            <li class="active">Dalam Perawatan: <b>{{alldata.active.value}}</b></li>
+            <li class="recovered">Sembuh: <b>{{alldata.recovered.value}}</b></li>
+            <li class="deaths">Meninggal: <b>{{alldata.deaths.value}}</b></li>
         </ul>
         <small class="covid-date">
             <!-- Update terakhir: {{alldata.metadata.lastUpdatedAt}} <br> -->
-            API dari <a href="https://indonesia-covid-19.mathdro.id/api" target="_blank" rel="noopener">Mathdroid</a>
+            API dari <a href="https://indonesia-covid-19.mathdro.id/api" target="_blank" rel="noopener">Mathdroid</a> <br>
+            Data berdasarkan dari situs resmi pemerintah Indonesia <a href="https://www.covid19.go.id/">covid19.go.id</a>
         </small>
     </div>
     </div>
@@ -52,6 +53,21 @@ export default {
         .covid
             h2
                 color: #51E3D4
+            ul
+                list-style: none
+                padding: 0
+                .confirmed
+                    b
+                        color: #f2c94c
+                .active
+                    b
+                        color: #f5a623
+                .recovered
+                    b
+                        color: #219653
+                .deaths
+                    b
+                        color: #d8232a
             .covid-date
                 a
                     color: #51E3D4
