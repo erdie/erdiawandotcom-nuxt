69fdae4219ad12a437660921968781398e9411e2
Erdiawan Anna <erdie.one@gmail.com>
2020-04-08 19:50:25 +0700

    add chart

---
+++ pages/covid/Chart.vue
@@ -0,0 +1,27 @@
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+  extends: Line,
+  props: {
+    chartdata: {
+      type: Object,
+      default: null
+    },
+    options: {
+      type: Object,
+      default: null
+    }
+  },
+  watch: {
+    chartdata(after) {
+      if (after != null) {
+        this.renderChart(this.chartdata, this.options)
+      }
+    }
+  },
+  // mounted () {
+  //   this.renderChart(this.chartdata, this.options)
+  // }
+}
+</script>
--- pages/covid/index.vue
+++ pages/covid/index.vue
@@ -1,25 +1,40 @@
 <template>
     <div class="covid">
         <div class="container">
-        <h2>Kasus Covid19 di Indonesia</h2>
-        <small>(Total kasus di Indonesia)</small>
-        <ul class="indonesia">
-            <li class="confirmed">Terkonfirmasi: <b>{{alldata.jumlahKasus}}</b></li>
-            <li class="active">Dalam Perawatan: <b>{{alldata.perawatan}}</b></li>
-            <li class="recovered">Sembuh: <b>{{alldata.sembuh}}</b></li>
-            <li class="deaths">Meninggal: <b>{{alldata.meninggal}}</b></li>
-        </ul>
-        <h3>Data Per Provinsi</h3>
-        <small>(Total kasus Per Provinsi)</small>
-        <input class="province-search" type="text" v-model="search" placeholder="Ketikkan nama provinsi">
-        <ul v-for="cases in filterList" :key="cases.id">
-            <h4>🔴{{cases.provinsi}}</h4>
-            <ul class="province">
-                <li class="confirmed">Terkonfirmasi Akumulatif: <strong>{{cases.kasusPosi}}</strong></li>
-                <li class="recovered">Sembuh Akumulatif: <strong>{{cases.kasusSemb}}</strong></li>
-                <li class="deaths">Meninggal Akumulatif: <strong>{{cases.kasusMeni}}</strong></li>
-            </ul> 
-        </ul>
+        <div class="covid-title">
+            <h1>Kasus Covid19 di Indonesia</h1>
+            <small>(Total kasus di Indonesia)</small>
+        </div>
+        <div class="item">
+            <div class="d:item__8 t:item__8 m:item__12">
+                <line-chart
+                v-if="loaded" 
+                :chartdata="chartdata"
+                :options="options"/>
+                <!-- <line-chart :chart-data="datacollection"></line-chart> -->
+                <ul class="indonesia">
+                    <li class="confirmed"><b>{{alldata.jumlahKasus}}</b><br>Terkonfirmasi</li>
+                    <li class="active"><b>{{alldata.perawatan}}</b><br>Dalam Perawatan</li>
+                    <li class="recovered"><b>{{alldata.sembuh}}</b><br>Sembuh</li>
+                    <li class="deaths"><b>{{alldata.meninggal}}</b><br>Meninggal</li>
+                </ul>
+            </div>
+            <div class="d:item__4 t:item__4 m:item__12">
+                <div class="province-group">
+                    <h3>Data Per Provinsi</h3>
+                    <small>(Total kasus Per Provinsi)</small>
+                    <input class="province-search" type="text" v-model="search" placeholder="Ketikkan nama provinsi">
+                    <ul v-for="cases in filterList" :key="cases.id">
+                        <h4>📍{{cases.provinsi}}</h4>
+                        <ul class="province">
+                            <li class="confirmed">Terkonfirmasi Akumulatif: <strong>{{cases.kasusPosi}}</strong></li>
+                            <li class="recovered">Sembuh Akumulatif: <strong>{{cases.kasusSemb}}</strong></li>
+                            <li class="deaths">Meninggal Akumulatif: <strong>{{cases.kasusMeni}}</strong></li>
+                        </ul> 
+                    </ul>
+                </div>
+            </div>
+        </div>
         <small class="covid-date">
             API dari <a href="https://github.com/mathdroid/indonesia-covid-19-api" target="_blank" rel="noopener">Mathdroid</a> <br>
             Data berdasarkan situs resmi pemerintah Indonesia <a href="https://www.covid19.go.id/">covid19.go.id</a>
@@ -30,15 +45,25 @@
 
 <script>
 import axios from 'axios'
-import 'vue-css-donut-chart/dist/vcdonut.css'
+import LineChart from './Chart'
 
 export default {
-    data: () => ({
-        loading: true,
-        title: 'Kasus Covid19 di Indonesia',
-        dummy: [{ value: 25 }, { value: 25 }],
-        search: ''
-    }),
+    name: 'LineChartContainer',
+    components: { LineChart },
+    layout: 'journal',
+    data() {
+        return {
+            loading: true,
+            title: 'Kasus Covid19 di Indonesia',
+            search: '',
+            loaded: true,
+            chartdata: null,
+            options: {
+                responsive: true,
+                maintainAspectRatio: false
+            }
+        }
+    },
     head () {
         return {
         title: this.title,
@@ -46,24 +71,89 @@ export default {
             { hid: 'description', name: 'description', content: 'Kasus Covid19 di Indonesia' }
         ]
     }},
-    layout: 'journal',
     async asyncData() {
         let firstURL = 'https://indonesia-covid-19-api.now.sh/api/'
         let secondURL = 'https://indonesia-covid-19-api.now.sh/api/provinsi'
+        let thirdURL = 'https://indonesia-covid-19.mathdro.id/api/harian'
 
         const firstResponse = await axios.get(firstURL)
         const secondResponse = await axios.get(secondURL)
+        const thirdResponse = await axios.get(thirdURL)
+
+        const compileKasusKumulatif = thirdResponse.data.data.map(compile => {
+            return compile.jumlahKasusKumulatif
+        })
+
+        const compileDalamPerawatan = thirdResponse.data.data.map(compile => {
+            return compile.jumlahpasiendalamperawatan
+        })
+
+        const compilePasienSembuh = thirdResponse.data.data.map(compile => {
+            return compile.jumlahPasienSembuh
+        })
+
+        const compilePasienMeninggal = thirdResponse.data.data.map(compile => {
+            return compile.jumlahPasienMeninggal
+        })
+
+        const compileHari = thirdResponse.data.data.map(compile => {
+            return compile.harike
+        })
+
+        //Convert Array to Object
+        // const compileKasusKumulatifObject = Object.assign({}, compileKasusKumulatif)
+
+        console.log(compileKasusKumulatif)
 
         return {
             alldata: firstResponse.data,
-            provincedata: secondResponse.data.data
+            provincedata: secondResponse.data.data,
+            dataKasusKumulatif: compileKasusKumulatif,
+            dataDalamPerawatan: compileDalamPerawatan,
+            dataPasienSembuh: compilePasienSembuh,
+            dataPasienMeninggal: compilePasienMeninggal,
+            dataLabels: compileHari
         }
+        
         asyncData().catch((error) => {
             console.log(error)
         })
     },
+    mounted () {
+      this.fillData()
+    },
+    methods: {
+        fillData () {
+            this.chartdata = {
+            labels: this.dataLabels,
+            datasets: [
+                {
+                label: 'Pasien Sembuh',
+                backgroundColor: '#219653',
+                data: this.dataPasienSembuh
+                },
+                {
+                label: 'Pasien Meninggal',
+                backgroundColor: '#d8232a',
+                data: this.dataPasienMeninggal
+                },
+                {
+                label: 'Pasien Dalam Perawatan',
+                backgroundColor: '#f5a623',
+                data: this.dataDalamPerawatan
+                },
+                {
+                label: 'Kasus Kumulatif',
+                backgroundColor: '#f2c94c',
+                data: this.dataKasusKumulatif
+                }
+            ]
+            }
+            this.loaded = true
+      }
+    },
     computed: {
-        filterList: function() {
+        filterList () {
             let filtering = this.provincedata
             if (this.search){
                 filtering = filtering.filter((cases) => {
@@ -81,7 +171,7 @@ export default {
         padding: 90px 0 10px 0
         // height: 70vh
         margin: 0 15px
-        h2
+        h1
             color: #5b64a0
             margin-bottom: 0
         h3
@@ -90,9 +180,23 @@ export default {
             margin: 0
         ul
             padding: 0
+        .covid-title
+            text-align: center
+            margin-bottom: 20px
         .indonesia
             list-style: none
             padding: 0
+            display: flex
+            text-align: center
+            width: 100%
+            li
+                flex: 1
+                padding: 10px 0 20px 0
+                margin: 10px
+                border-radius: 5px
+                box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)
+                b
+                    font-size: 30px
             .confirmed
                 b
                     color: #f2c94c
@@ -105,6 +209,10 @@ export default {
             .deaths
                 b
                     color: #d8232a
+        .province-group
+            height: 520px
+            overflow-y: scroll
+            padding-left: 10px
         .province-search
             display: block
             margin: 10px 0
