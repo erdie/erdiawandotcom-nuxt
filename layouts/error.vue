08e176ddaa9b51b04d6184bd7e53cfd5e92849be
Erdiawan Anna <erdie.one@gmail.com>
2020-05-20 04:59:11 +0700

    fixing 404 page, remove old blog page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,46 +1,89 @@
 <template>
-  <section>
-    <div class="container-card">
-      <div class="error-status">
-        <img src="~/assets/img/erdiawan-2.svg" alt="Erdiawan" class="logo" />
-        <h1 class="title">
-          {{ error.statusCode }}
-        </h1>
-        <h2 class="info">
-          {{ error.message }}
-        </h2>
-        <nuxt-link class="button" to="/" v-if="error.statusCode === 404">
-          &#60; Back to Homepage
-        </nuxt-link>
-      </div>
+  <div v-if="loading">
+    <Navigation />
+    <NavigationMobile />
+    <div class="error-status">
+        <div class="error-status-group">
+            <h1 class="title">
+            {{ error.statusCode }}
+            </h1>
+            <h2 class="info">
+            {{ error.message }}
+            </h2>
+            <nuxt-link class="button" to="/" v-if="error.statusCode === 404">
+            &#60; Back to Homepage
+            </nuxt-link>
+        </div>
     </div>
-  </section>
+    <Footer />
+  </div>
 </template>
+
 <script>
+import Navigation from '~/components/Navigation.vue'
+import NavigationMobile from '~/components/Navigation-Mobile.vue'
+import Footer from '~/components/Footer.vue'
+
 export default {
-  props: ['error']
+  props: ['error'],
+  components: {
+      Navigation,
+      NavigationMobile,
+      Footer
+  },
+  data: () => ({
+    loading: true,
+  }),
+  methods: {
+    start () {
+      this.loading = true
+    },
+    finish () {
+      this.loading = false
+    }
+  }
 }
 </script>
 
 <style lang="sass" scoped>
-  .error-status
-    border-radius: 15px
-    background-color: white
-    text-align: center
-    padding: 25px
-    margin: 30px 0
-    .title
-      margin-top: 15px
-      font-size: 5em
-      margin-bottom: 10px
-    .info
-      font-weight: 300
-      color: #9aabb1
-      font-size: 20px
-      margin: 0 0 20px 0
-    .button
-      margin-top: 50px
-      text-decoration: none
-      color: #333
-      font-weight: bold
+    .error-status
+        background-color: white
+        text-align: center
+        padding: 25px
+        height: 80vh
+        display: flex
+        justify-content: center
+        align-items: center
+        .error-status-group
+            .title
+                margin-top: 15px
+                font-size: 8em
+                margin-bottom: 0px
+                color: #474F85
+            .info
+                font-weight: 300
+                color: #212121
+                font-size: 20px
+                margin: 0 0 40px 0
+            .button
+                margin-top: 50px
+                text-decoration: none
+                color: #333
+                font-weight: bold
+                color: #474F85
+            .button:hover
+                color: #51E3D4
+
+    @media (prefers-color-scheme: dark)
+        .error-status
+            background-color: black
+            .error-status-group
+                .title
+                    color: #51E3D4
+                .info
+                    color: white
+                .button
+                    color: #51E3D4
+                .button:hover
+                    color: #474F85
 </style>
--- plugins/buttercms.js
+++
@@ -1,4 +0,0 @@
-export const butterService = Butter()
-export const butter = Butter('b1b4910662f0264ee73a62f21934b2e0a2c84d4e')
-
-import { butter } from '~/plugins/buttercms'
