02efada33bcb5ae93b719b2cde2b63da0d5d82cb
Erdiawan Anna <erdie.one@gmail.com>
2020-05-20 16:02:39 +0700

    remove unnecessary package, reorder portfolio card

--- components/PortfolioGrid.vue
+++ components/PortfolioGrid.vue
@@ -32,21 +32,22 @@
                     </p>
                 </div>
                 <div class="portfolio-card d:item__6 t:item__6 m:item__12">
-                    <img src="~/assets/img/portfolio/digitarasa.png" loading="lazy" alt="Digitarasa by GoFood">
-                    <h3>Digitarasa by GoFood</h3>
-                    <p>Tech Stack: HTML5, CSS3, SASS<br>
-                        Frontend Dev: Anna Erdiawan<br>
-                        Demo: <a href="https://digitarasa.id" target="_blank" rel="noopener">digitarasa.id</a><br>
-                        Source Code: <strong>Private</strong>
-                    </p>
-                </div>
-                <div class="portfolio-card d:item__6 t:item__6 m:item__12">
                     <img src="~/assets/img/portfolio/devfest-jakarta.png" loading="lazy" alt="DevFest 2019 Jakarta">
                     <h3>DevFest 2019 Jakarta</h3>
                     <p>Tech Stack: HTML5, CSS3, SASS, NUXT, VUE JS<br>
                         Frontend Dev: Anna Erdiawan<br>
                         Demo: <a href="https://devfest.gdgjakarta.org" target="_blank" rel="noopener">devfest.gdgjakarta.org</a><br>
                         Source Code: <a href="https://github.com/gdgindonesia/devfest-19-landing" target="_blank">Github</a>
+                    </p>
+                </div>
+                <div class="portfolio-card d:item__6 t:item__6 m:item__12">
+                    <img src="~/assets/img/portfolio/digitaraya.png" loading="lazy" alt="Digitaraya">
+                    <h3>Digitaraya</h3>
+                    <p>Tech Stack: HTML5, CSS3, SASS, JQUERY<br>
+                        Frontend Dev: Anna Erdiawan<br>
+                        Demo: <a href="https://digitaraya.com" target="_blank" rel="noopener">digitaraya.com</a><br>
+                        <!-- Demo v1: <a href="https://v1.digitaraya.com" target="_blank" rel="noopener">v1.digitaraya.com</a> (Wordpress) <br> -->
+                        Source Code: <strong>Private</strong>
                     </p>
                 </div>
                 <div class="portfolio-card d:item__6 t:item__6 m:item__12">
@@ -61,126 +62,125 @@
               </div>
               <transition name="fade" mode="out-in">
                 <div class="portfolio-grid item" key="3" v-show="isShowing">
-                  <div class="portfolio-card d:item__6 t:item__6 m:item__12">
-                      <img src="~/assets/img/portfolio/founder-asia.png" loading="lazy" alt="Startup Pilihan Tempo">
-                      <h3>Founder Asia</h3>
-                      <p>Tech Stack: HTML5, CSS3, SASS, JQUERY<br>
-                          Frontend Dev: Anna Erdiawan<br>
-                          Demo: <a href="https://founderasia.com/" target="_blank" rel="noopener">founderasia.com</a> <br>
-                          Source Code: <strong> Private</strong>
-                      </p>
-                  </div>
-                  <div class="portfolio-card d:item__6 t:item__6 m:item__12">
-                      <img src="~/assets/img/portfolio/exp-up.png" loading="lazy" alt="EXP-UP! Academy Esport">
-                      <h3>EXP Esport Academy</h3>
-                      <p>Tech Stack: HTML5, CSS3, SASS, JQUERY<br>
-                          Frontend Dev: Anna Erdiawan<br>
-                          Demo: <a href="https://exp-up.id" target="_blank" rel="noopener">exp-up.id</a> & <a href="https://exp-academy.id" target="_blank" rel="noopener">exp-academy.id</a><br>
-                          Source Code: <strong>Private</strong>
-                      </p>
-                  </div>
-                  <div class="portfolio-card d:item__6 t:item__6 m:item__12">
-                      <img src="~/assets/img/portfolio/spt.png" loading="lazy" alt="Startup Pilihan Tempo">
-                      <h3>Startup Pilihan Tempo</h3>
-                      <p>Tech Stack: HTML5, CSS3, SASS, JQUERY<br>
-                          Frontend Dev: Anna Erdiawan<br>
-                          Demo: <a href="https://digitaraya.com/startuppilihantempo/" target="_blank" rel="noopener">digitaraya.com/startuppilihantempo/</a><br>
-                          Source Code: <strong>Private</strong>
-                      </p>
-                  </div>
-                  <div class="portfolio-card d:item__6 t:item__6 m:item__12">
-                      <img src="~/assets/img/portfolio/digitaraya.png" loading="lazy" alt="Digitaraya">
-                      <h3>Digitaraya</h3>
-                      <p>Tech Stack: HTML5, CSS3, SASS, JQUERY<br>
-                          Frontend Dev: Anna Erdiawan<br>
-                          Demo: <a href="https://digitaraya.com" target="_blank" rel="noopener">digitaraya.com</a><br>
-                          Demo v1: <a href="https://v1.digitaraya.com" target="_blank" rel="noopener">v1.digitaraya.com</a> (Wordpress) <br>
-                          Source Code: <strong>Private</strong>
-                      </p>
-                  </div>
-                  <div class="portfolio-card d:item__6 t:item__6 m:item__12">
-                      <img src="~/assets/img/portfolio/digitaraya-tourism.jpg" loading="lazy" alt="Digitaraya Tourism Batch (One Pager)">
-                      <h3>Digitaraya Tourism Batch (One Pager)</h3>
-                      <p>Tech Stack: HTML5, CSS3, SASS, JQUERY<br>
-                          Frontend Dev: Anna Erdiawan<br>
-                          Demo: <a href="https://digitaraya.com/tiketcom-tourismbatch/" target="_blank" rel="noopener">digitaraya.com/tiketcom-tourismbatch/</a> <br>
-                          Source Code: <strong>Private</strong>
-                      </p>
-                  </div>
-                  <div class="portfolio-card d:item__6 t:item__6 m:item__12">
-                      <img src="~/assets/img/portfolio/simona.jpg" loading="lazy" alt="Simona Ventures">
-                      <h3>Simona Ventures</h3>
-                      <p>Tech Stack: SASS, HTML5, CSS3, JQUERY<br>
-                          Frontend Dev: Anna Erdiawan<br>
-                          Demo: <a href="https://simona.ventures" target="_blank" rel="noopener">simona.ventures</a><br>
-                          Source Code: <strong>Private</strong>
-                      </p>
-                  </div>
-                  <div class="portfolio-card d:item__6 t:item__6 m:item__12">
-                      <img src="~/assets/img/portfolio/simona-apacwomen.jpg" loading="lazy" alt="Simona Venture APAC Women ">
-                      <h3>Simona Venture APAC Women (One Pager)</h3>
-                      <p>Tech Stack: SASS, HTML5, CSS3, JQUERY <br>
-                          Frontend Dev: Anna Erdiawan<br>
-                          Demo: <a href="https://simona.ventures/apacwomen/" target="_blank" rel="noopener">simona.ventures/apacwomen/</a><br>
-                          Source Code: <strong>Private</strong>
-                      </p>
-                  </div>
-                  <div class="portfolio-card d:item__6 t:item__6 m:item__12">
-                      <img src="~/assets/img/portfolio/lancong.jpg" loading="lazy" alt="Simona Ventures">
-                      <h3>Lancong.id</h3>
-                      <p>Tech Stack: SASS, HTML5, CSS3, JQUERY<br>
-                          Frontend Dev: Anna Erdiawan<br>
-                          Demo: <a href="http://lancong.id" target="_blank" rel="noopener">lancong.id</a><br>
-                          Source Code: <strong>Private</strong>
-                      </p>
-                  </div>
-                  <div class="portfolio-card d:item__6 t:item__6 m:item__12">
-                      <img src="~/assets/img/portfolio/kinesys.jpg" loading="lazy" alt="Kinesys Ventures">
-                      <h3>Kinesys Ventures</h3>
-                      <p>Tech Stack: SASS, HTML5, CSS3, JQUERY <br>
-                          Frontend Dev: Anna Erdiawan<br>
-                          Demo: <a href="https://kinesysgroup.com" target="_blank" rel="noopener">kinesysgroup.com</a><br>
-                          Source Code: <strong>Private</strong>
-                      </p>
-                  </div>
-                  <div class="portfolio-card d:item__6 t:item__6 m:item__12">
-                      <img src="~/assets/img/portfolio/mariviu.jpg" loading="lazy" alt="Simona Ventures">
-                      <h3>Mariviu</h3>
-                      <p>Tech Stack: WORDPRESS<br>
-                          Frontend Dev: Anna Erdiawan<br>
-                          Demo: <a href="https://mariviu.com" target="_blank" rel="noopener">mariviu.com</a> <br>
-                          Source Code: <strong>Private</strong>
-                      </p>
-                  </div>
-                  <div class="portfolio-card d:item__6 t:item__6 m:item__12">
-                      <img src="~/assets/img/portfolio/chromplex.jpg" loading="lazy" alt="Chromplex">
-                      <h3>Chromplex</h3>
-                      <p>Tech Stack: WORDPRESS, MATERIAL LITE DESIGN<br>
-                          Frontend Dev: Anna Erdiawan<br>
-                          Demo: <a href="https://chromplex.com" target="_blank" rel="noopener">chromplex.com</a> <br>
-                          Source Code: <strong>Private</strong>
-                      </p>
-                  </div>
-                  <div class="portfolio-card d:item__6 t:item__6 m:item__12">
-                      <img src="~/assets/img/portfolio/digiskul.jpg" loading="lazy" alt="Simona Ventures">
-                      <h3>Digiskul</h3>
-                      <p>Tech Stack: HTML5, CSS3, VUE JS<br>
-                          Frontend Dev: Anna Erdiawan<br>
-                          Backend Dev: Rio Swarawan & Ali Mahfud<br>
-                          Demo: <a href="http://digiskul.nuansa.id" target="_blank" rel="noopener">digiskul.nuansa.id</a><br>
-                          Source Code: <strong>Private</strong>
-                      </p>
-                  </div>
-                  <div class="portfolio-card d:item__6 t:item__6 m:item__12">
-                      <img src="~/assets/img/portfolio/perkim-sumbar.png" loading="lazy" alt="Perkim Sumbar">
-                      <h3>Perkim Sumatera Barat</h3>
-                      <p>Tech Stack: HTML5, CSS3, BOOTSTRAP<br>
-                          Frontend Dev: Anna Erdiawan<br>
-                          Demo: <a href="http://perkim.sumbarprov.go.id" target="_blank" rel="noopener">perkim.sumbarprov.go.id</a><br>
-                          Source Code: <strong>Private</strong>
-                      </p>
-                  </div>
-              </div>
+                    <div class="portfolio-card d:item__6 t:item__6 m:item__12">
+                        <img src="~/assets/img/portfolio/digitarasa.png" loading="lazy" alt="Digitarasa by GoFood">
+                        <h3>Digitarasa by GoFood</h3>
+                        <p>Tech Stack: HTML5, CSS3, SASS<br>
+                            Frontend Dev: Anna Erdiawan<br>
+                            Demo: <a href="https://digitarasa.id" target="_blank" rel="noopener">digitarasa.id</a><br>
+                            Source Code: <strong>Private</strong>
+                        </p>
+                    </div>
+                    <div class="portfolio-card d:item__6 t:item__6 m:item__12">
+                        <img src="~/assets/img/portfolio/founder-asia.png" loading="lazy" alt="Startup Pilihan Tempo">
+                        <h3>Founder Asia</h3>
+                        <p>Tech Stack: HTML5, CSS3, SASS, JQUERY<br>
+                            Frontend Dev: Anna Erdiawan<br>
+                            Demo: <a href="https://founderasia.com/" target="_blank" rel="noopener">founderasia.com</a> <br>
+                            Source Code: <strong> Private</strong>
+                        </p>
+                    </div>
+                    <div class="portfolio-card d:item__6 t:item__6 m:item__12">
+                        <img src="~/assets/img/portfolio/exp-up.png" loading="lazy" alt="EXP-UP! Academy Esport">
+                        <h3>EXP Esport Academy</h3>
+                        <p>Tech Stack: HTML5, CSS3, SASS, JQUERY<br>
+                            Frontend Dev: Anna Erdiawan<br>
+                            Demo: <a href="https://exp-up.id" target="_blank" rel="noopener">exp-up.id</a> & <a href="https://exp-academy.id" target="_blank" rel="noopener">exp-academy.id</a><br>
+                            Source Code: <strong>Private</strong>
+                        </p>
+                    </div>
+                    <div class="portfolio-card d:item__6 t:item__6 m:item__12">
+                        <img src="~/assets/img/portfolio/spt.png" loading="lazy" alt="Startup Pilihan Tempo">
+                        <h3>Startup Pilihan Tempo</h3>
+                        <p>Tech Stack: HTML5, CSS3, SASS, JQUERY<br>
+                            Frontend Dev: Anna Erdiawan<br>
+                            Demo: <a href="https://digitaraya.com/startuppilihantempo/" target="_blank" rel="noopener">digitaraya.com/startuppilihantempo/</a><br>
+                            Source Code: <strong>Private</strong>
+                        </p>
+                    </div>
+                    <div class="portfolio-card d:item__6 t:item__6 m:item__12">
+                        <img src="~/assets/img/portfolio/digitaraya-tourism.jpg" loading="lazy" alt="Digitaraya Tourism Batch (One Pager)">
+                        <h3>Digitaraya Tourism Batch (One Pager)</h3>
+                        <p>Tech Stack: HTML5, CSS3, SASS, JQUERY<br>
+                            Frontend Dev: Anna Erdiawan<br>
+                            Demo: <a href="https://digitaraya.com/tiketcom-tourismbatch/" target="_blank" rel="noopener">digitaraya.com/tiketcom-tourismbatch/</a> <br>
+                            Source Code: <strong>Private</strong>
+                        </p>
+                    </div>
+                    <div class="portfolio-card d:item__6 t:item__6 m:item__12">
+                        <img src="~/assets/img/portfolio/simona.jpg" loading="lazy" alt="Simona Ventures">
+                        <h3>Simona Ventures</h3>
+                        <p>Tech Stack: SASS, HTML5, CSS3, JQUERY<br>
+                            Frontend Dev: Anna Erdiawan<br>
+                            Demo: <a href="https://simona.ventures" target="_blank" rel="noopener">simona.ventures</a><br>
+                            Source Code: <strong>Private</strong>
+                        </p>
+                    </div>
+                    <div class="portfolio-card d:item__6 t:item__6 m:item__12">
+                        <img src="~/assets/img/portfolio/simona-apacwomen.jpg" loading="lazy" alt="Simona Venture APAC Women ">
+                        <h3>Simona Venture APAC Women (One Pager)</h3>
+                        <p>Tech Stack: SASS, HTML5, CSS3, JQUERY <br>
+                            Frontend Dev: Anna Erdiawan<br>
+                            Demo: <a href="https://simona.ventures/apacwomen/" target="_blank" rel="noopener">simona.ventures/apacwomen/</a><br>
+                            Source Code: <strong>Private</strong>
+                        </p>
+                    </div>
+                    <div class="portfolio-card d:item__6 t:item__6 m:item__12">
+                        <img src="~/assets/img/portfolio/lancong.jpg" loading="lazy" alt="Simona Ventures">
+                        <h3>Lancong.id</h3>
+                        <p>Tech Stack: SASS, HTML5, CSS3, JQUERY<br>
+                            Frontend Dev: Anna Erdiawan<br>
+                            Demo: <a href="http://lancong.id" target="_blank" rel="noopener">lancong.id</a><br>
+                            Source Code: <strong>Private</strong>
+                        </p>
+                    </div>
+                    <div class="portfolio-card d:item__6 t:item__6 m:item__12">
+                        <img src="~/assets/img/portfolio/kinesys.jpg" loading="lazy" alt="Kinesys Ventures">
+                        <h3>Kinesys Ventures</h3>
+                        <p>Tech Stack: SASS, HTML5, CSS3, JQUERY <br>
+                            Frontend Dev: Anna Erdiawan<br>
+                            Demo: <a href="https://kinesysgroup.com" target="_blank" rel="noopener">kinesysgroup.com</a><br>
+                            Source Code: <strong>Private</strong>
+                        </p>
+                    </div>
+                    <div class="portfolio-card d:item__6 t:item__6 m:item__12">
+                        <img src="~/assets/img/portfolio/mariviu.jpg" loading="lazy" alt="Simona Ventures">
+                        <h3>Mariviu</h3>
+                        <p>Tech Stack: WORDPRESS<br>
+                            Frontend Dev: Anna Erdiawan<br>
+                            Demo: <a href="https://mariviu.com" target="_blank" rel="noopener">mariviu.com</a> <br>
+                            Source Code: <strong>Private</strong>
+                        </p>
+                    </div>
+                    <div class="portfolio-card d:item__6 t:item__6 m:item__12">
+                        <img src="~/assets/img/portfolio/chromplex.jpg" loading="lazy" alt="Chromplex">
+                        <h3>Chromplex</h3>
+                        <p>Tech Stack: WORDPRESS, MATERIAL LITE DESIGN<br>
+                            Frontend Dev: Anna Erdiawan<br>
+                            Demo: <a href="https://chromplex.com" target="_blank" rel="noopener">chromplex.com</a> <br>
+                            Source Code: <strong>Private</strong>
+                        </p>
+                    </div>
+                    <div class="portfolio-card d:item__6 t:item__6 m:item__12">
+                        <img src="~/assets/img/portfolio/digiskul.jpg" loading="lazy" alt="Simona Ventures">
+                        <h3>Digiskul</h3>
+                        <p>Tech Stack: HTML5, CSS3, VUE JS<br>
+                            Frontend Dev: Anna Erdiawan<br>
+                            Backend Dev: Rio Swarawan & Ali Mahfud<br>
+                            Demo: <a href="http://digiskul.nuansa.id" target="_blank" rel="noopener">digiskul.nuansa.id</a><br>
+                            Source Code: <strong>Private</strong>
+                        </p>
+                    </div>
+                    <div class="portfolio-card d:item__6 t:item__6 m:item__12">
+                        <img src="~/assets/img/portfolio/perkim-sumbar.png" loading="lazy" alt="Perkim Sumbar">
+                        <h3>Perkim Sumatera Barat</h3>
+                        <p>Tech Stack: HTML5, CSS3, BOOTSTRAP<br>
+                            Frontend Dev: Anna Erdiawan<br>
+                            Demo: <a href="http://perkim.sumbarprov.go.id" target="_blank" rel="noopener">perkim.sumbarprov.go.id</a><br>
+                            Source Code: <strong>Private</strong>
+                        </p>
+                    </div>
+                </div>
             </transition>
             <button @click="isShowing = !isShowing" v-show="!isShowing" class="btn btn-primary btn-show">
               Show All
