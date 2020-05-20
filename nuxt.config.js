734aaeea8a8a51816ddc6ef524a410e125e1bb09
Erdiawan Anna <erdie.one@gmail.com>
2020-05-21 06:56:44 +0700

    update sitemap config

--- nuxt.config.js
+++ nuxt.config.js
@@ -72,7 +72,17 @@ export default {
     exclude: [
       '/covid/**'
     ],
-    routes: []
+    routes: [],
+    routes: function() {
+        const fs = require('fs')
+        const path = require('path')
+        return fs.readdirSync('./assets/content/blog').map(file => {
+            return {
+                route: `/blog/${path.parse(file).name}`, // Return the slug
+                payload: require(`./assets/content/blog/${file}`),
+            }
+        })
+    }
   },
   robots: {
     UserAgent: '*',
