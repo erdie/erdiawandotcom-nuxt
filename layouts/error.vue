75bed5afb263cd69c0669bc6a0ee02b02147a596
Erdiawan Anna <erdie.one@gmail.com>
2020-05-27 23:44:01 +0700

    fixing dark mode for error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -74,7 +74,48 @@ export default {
             .button:hover
                 color: #51E3D4
 
+    .light-mode body
+        .error-status
+            background-color: white
+            text-align: center
+            padding: 25px
+            height: 80vh
+            display: flex
+            justify-content: center
+            align-items: center
+            .error-status-group
+                .title
+                    margin-top: 15px
+                    font-size: 8em
+                    margin-bottom: 0px
+                    color: #474F85
+                .info
+                    font-weight: 300
+                    color: #212121
+                    font-size: 20px
+                    margin: 0 0 40px 0
+                .button
+                    margin-top: 50px
+                    text-decoration: none
+                    color: #333
+                    font-weight: bold
+                    color: #474F85
+                .button:hover
+                    color: #51E3D4
+
     @media (prefers-color-scheme: dark)
+        .error-status
+            background-color: black
+            .error-status-group
+                .title
+                    color: #51E3D4
+                .info
+                    color: white
+                .button
+                    color: #51E3D4
+                .button:hover
+                    color: #474F85
+    .dark-mode body
         .error-status
             background-color: black
             .error-status-group
